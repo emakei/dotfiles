3ceafcc0f64d8dfb6724d0742e24fdcad7280676
Yauhen Makei <yauhen.makei@gmail.com>
2026-05-30 00:00:41 +0400

    Немного изменил настройки под свои нужны

--- config/nvim/lua/neovide.lua
+++ config/nvim/lua/neovide.lua
@@ -2,9 +2,10 @@
 
 if vim.g.neovide then
     -- Neovide specific settings
-    vim.o.guifont = "JetBrainsMono Nerd Font:h14"
-    vim.g.neovide_transparency = 0.95
+    vim.o.guifont = "JetBrains Mono:h14"
+    vim.g.neovide_opacity = 0.95
     vim.g.neovide_cursor_animation_length = 0.13
     vim.g.neovide_cursor_trail_size = 0.8
+    vim.g.neovide_hide_mouse_when_typing = true
 end
 
