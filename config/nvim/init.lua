b1a9dfe5bda17d28fd5e5aee3070e8b54c2bb71b
Yauhen Makei <yauhen.makei@gmail.com>
2026-05-29 23:15:21 +0400

    GEMINI refactoring

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -5,28 +5,15 @@ require("lazy-setup")
 require("lsp")
 require("neovide")
 
--- Shared global function so autocmds.lua can access it later
-_G.sync_theme = function()
-    local is_mac = vim.fn.has("macunix") == 1
-    local background = 'light'
-
-    if is_mac then
-        local macos_theme = vim.fn.system("defaults read -g AppleInterfaceStyle 2>/dev/null")
-        if macos_theme:find("Dark") then
-            background = 'dark'
-        end
-    end
-
-    vim.o.background = background
-end
+local utils = require("utils")
 
 -- Run once on startup
-_G.sync_theme()
+utils.sync_theme()
 
 -- Safely load solarized
 local status_ok, solarized = pcall(require, "solarized")
 if status_ok then
-    solarized.setup(opts)
+    solarized.setup() -- Removed 'opts' as it was undefined
     vim.cmd.colorscheme 'solarized'
 else
     vim.cmd.colorscheme 'default'
--- config/nvim/lua/autocmds.lua
+++ config/nvim/lua/autocmds.lua
@@ -1,5 +1,6 @@
+local utils = require("utils")
+
 -- Trigger checktime when files changes on disk
--- https://unix.stackexchange.com/questions/149209/refresh-changed-content-of-file-opened-in-vim/383044#383044
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold", "CursorHoldI" }, {
   callback = function()
     if vim.fn.getcmdwintype() == "" then
@@ -17,7 +18,6 @@ vim.api.nvim_create_autocmd("FileChangedShellPost", {
 
 -- Command to compare buffer with file on disk
 vim.api.nvim_create_user_command("DiffOrig", function()
-  -- Create a new vertical split with the contents of the file on disk
   vim.cmd("vert new | set bt=nofile | r ++edit # | 0d_ | diffthis | wincmd p | diffthis")
 end, { desc = "Compare current buffer with file on disk" })
 
@@ -27,32 +27,19 @@ vim.api.nvim_create_autocmd("CursorHold", {
 	end
 })
 
--- Guard clause in case autocmds loads before the function is defined
-if _G.sync_theme then
-    vim.api.nvim_create_autocmd({ "FocusGained", "VimResume" }, {
-        group = vim.api.nvim_create_augroup("OSThemeSync", { clear = true }),
-        callback = function()
-            _G.sync_theme()
-        end,
-    })
-end
+-- OS Theme Sync
+vim.api.nvim_create_autocmd({ "FocusGained", "VimResume" }, {
+    group = vim.api.nvim_create_augroup("OSThemeSync", { clear = true }),
+    callback = function()
+        utils.sync_theme()
+    end,
+})
 
 -- Terminal keymaps
-function _G.set_terminal_keymaps()
-  local opts = {buffer = 0}
-  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
-end
-
 vim.api.nvim_create_autocmd("TermOpen", {
   pattern = "term://*",
   callback = function()
-    set_terminal_keymaps()
+    utils.set_terminal_keymaps()
     vim.cmd("startinsert")
   end,
 })
